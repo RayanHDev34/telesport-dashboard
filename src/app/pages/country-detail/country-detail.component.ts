09777fe43f1beb71a68f46ea0b2991a62fc5c42d
Rayan H. <rayan.hasnoun34@gmail.com>
2025-09-12 16:33:49 +0200

    feat: update CountryDetailComponent to display country statistics with dynamic data from ParticipationsService

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,8 +1,13 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+
 import { OlympicCountry } from 'src/app/core/models/Olympic';
+
+import { ParticipationsService } from 'src/app/core/services/participations.service';
+
 import { StatCardComponent } from 'src/app/shared/components/stat-card/stat-card.component';
+
 
 @Component({
   selector: 'app-country-detail',
@@ -12,19 +17,21 @@ import { StatCardComponent } from 'src/app/shared/components/stat-card/stat-card
   styleUrls: ['./country-detail.component.scss'],
 })
 export class CountryDetailComponent {
-  country: OlympicCountry | null = null;
-  totalMedals = 0;
-
-  constructor(private router: Router) {
+  country!: OlympicCountry;
+  medals!: number;
+  entries!: number;
+  athletes!: number;
+  
+  constructor(private router: Router, private participations: ParticipationsService) {
     // 👉 récupérer l'objet envoyé dans router.navigate(..., { state: { country } })
     const nav = this.router.getCurrentNavigation();
     this.country = nav?.extras.state?.['country'] ?? null;
-    console.log('Country detail for:', this.country);
-    if (this.country) {
-      this.totalMedals = this.country.participations.reduce(
-        (acc, p) => acc + p.medalsCount,
-        0
-      );
-    }
   }
+
+  ngOnInit() {
+    this.entries = this.participations.getTotalEntries(this.country.participations)!;
+    this.medals = this.participations.getTotalMedals(this.country.participations)!;
+    this.athletes = this.participations.getTotalAthletes(this.country.participations)!;
+  }
+
 }
